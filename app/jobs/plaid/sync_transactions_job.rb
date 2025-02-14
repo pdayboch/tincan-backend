ea7de53358936ac83e1169cfd40386d1260accb7
Philip Dayboch <philip245@gmail.com>
2025-02-14 12:02:45 -0500

    Add Plaid SyncTransactionsJob
    This commit adds a new background job responsible for identifying
    Plaid Items that need a transcation sync.
    
    The current Sync period is set to 6 hours. Additionally, we will
    not attempt to sync transactions on Items that never synced
    accounts.
    
    This job has the option to specify specific Item IDs to sync
    transactions, or by default, the job will look for Plaid Items that
    have never had their transactions synced (new items), or haven't been
    fetched in over 6 hours.

---
+++ app/jobs/plaid/sync_transactions_job.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Plaid
+  class SyncTransactionsJob
+    include Sidekiq::Worker
+    include Sidekiq::Status::Worker
+
+    sidekiq_options retry: 2
+
+    SYNC_PERIOD = 6.hours
+
+    # @param override_item_ids [Array<String>, nil] optional item_ids to sync transactions.
+    # To sync for all items that need a transactions sync, pass nil
+    def perform(override_item_ids = nil)
+      @override_item_ids = override_item_ids
+      @category_mapper = Plaid::CategoryMapper.new
+      catch(:rate_limit_exceeded) do
+        items_to_sync.find_each(batch_size: 50) { |item| process_item(item) }
+      end
+    end
+
+    private
+
+    def process_item(item)
+      PlaidItem.transaction do
+        item.lock!
+        PlaidServices::Transactions::SyncService.new(
+          item,
+          category_mapper: @category_mapper
+        ).call
+      rescue Plaid::ApiRateLimitError
+        Rails.logger.warn('Plaid API rate limit exceeded. Stopping job.')
+        throw :rate_limit_exceeded
+      end
+    end
+
+    def items_to_sync
+      base_scope.lock('FOR UPDATE SKIP LOCKED')
+    end
+
+    def base_scope
+      if @override_item_ids
+        PlaidItem.where(item_id: @override_item_ids)
+      else
+        items_needing_transaction_sync
+      end
+    end
+
+    def items_needing_transaction_sync
+      PlaidItem
+        .where.not(accounts_synced_at: nil)
+        .where('transactions_synced_at IS NULL OR transactions_synced_at < ?', SYNC_PERIOD.ago)
+    end
+  end
+end
---
+++ test/jobs/plaid/sync_transactions_job_test.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Plaid
+  class SyncTransactionsJobTest < ActiveSupport::TestCase
+    test 'processes items with transactions_synced_at nil' do
+      item = plaid_items(:transactions_synced_nil)
+
+      mock_sync_service = mock('sync-service')
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .at_least_once
+                                              .returns(mock_sync_service)
+
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .with { |arg| arg.id == item.id }
+                                              .returns(mock_sync_service)
+
+      mock_sync_service.expects(:call)
+                       .at_least_once
+                       .returns(true)
+
+      Plaid::SyncTransactionsJob.new.perform
+    end
+
+    test 'processes items with transactions_synced_at older than 24 hours' do
+      item = plaid_items(:transactions_synced_older_6_h)
+
+      mock_sync_service = mock('sync-service')
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .at_least_once
+                                              .returns(mock_sync_service)
+
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .with { |arg| arg.id == item.id }
+                                              .returns(mock_sync_service)
+
+      mock_sync_service.expects(:call)
+                       .at_least_once
+                       .returns(true)
+
+      Plaid::SyncTransactionsJob.new.perform
+    end
+
+    test 'does not process items without accounts synced' do
+      item = plaid_items(:new_item)
+
+      mock_sync_service = mock('sync-service')
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .at_least_once
+                                              .returns(mock_sync_service)
+
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .with { |arg| arg.id == item.id }
+                                              .never
+
+      mock_sync_service.expects(:call)
+                       .at_least_once
+                       .returns(true)
+
+      Plaid::SyncTransactionsJob.new.perform
+    end
+
+    test 'processes only override_item_ids when specified' do
+      process_item = plaid_items(:transactions_synced_older_6_h)
+      not_process_item = plaid_items(:transactions_synced_nil)
+
+      mock_sync_service = mock('sync-service')
+
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .with { |arg| arg.id == process_item.id }
+                                              .returns(mock_sync_service)
+
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .with { |arg| arg.id == not_process_item.id }
+                                              .never
+
+      mock_sync_service.expects(:call)
+                       .at_least_once
+                       .returns(true)
+
+      Plaid::SyncTransactionsJob.new.perform([process_item.item_id])
+    end
+
+    test 'halts processing when plaid rate limit exceeded' do
+      mock_sync_service = mock('sync-service')
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .returns(mock_sync_service)
+
+      mock_sync_service.expects(:call)
+                       .raises(Plaid::ApiRateLimitError)
+
+      Rails.logger.expects(:warn).with('Plaid API rate limit exceeded. Stopping job.')
+
+      Plaid::SyncTransactionsJob.new.perform
+    end
+
+    test 'raises exception for plaid api error other than rate limit exceeded' do
+      some_error = Plaid::ApiError.new(
+        data: { 'error_type' => 'SERVER_ERROR' }
+      )
+      mock_sync_service = mock('sync-service')
+      PlaidServices::Transactions::SyncService.expects(:new)
+                                              .returns(mock_sync_service)
+
+      mock_sync_service.expects(:call)
+                       .raises(some_error)
+
+      assert_raises Plaid::ApiError do
+        Plaid::SyncTransactionsJob.new.perform
+      end
+    end
+  end
+end
